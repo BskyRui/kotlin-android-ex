7dec94f19e955c69016366d6bcc53159f925fc24
LiRui <bskyrui@msn.cn>
2019-07-12 11:07:30 +0800

    update build.gradle & create retrofit instance

--- library/src/main/java/indi/lirui/library/net/RestCreator.kt
+++ library/src/main/java/indi/lirui/library/net/RestCreator.kt
@@ -2,12 +2,40 @@ package indi.lirui.library.net
 
 import indi.lirui.library.global.GlobalKeys
 import indi.lirui.library.global.Mall
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
+import java.util.concurrent.TimeUnit
 
 
 // 创建Retrofit实例
 class RestCreator {
 
+    private object OkHttpHolder {
+        private const val TIME_OUT = 60
+        private val BUILDER = OkHttpClient.Builder()
+        internal val OK_HTTP_CLIENT = BUILDER
+            .connectTimeout(TIME_OUT.toLong(), TimeUnit.SECONDS)
+            .build()
+    }
+
     private object RetrofitHolder{
         private val BASE_URL: String = Mall.getConfiguration(GlobalKeys.API_HOST)
+        // 模块内可见
+        internal val RETROFIT_CLIENT = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(OkHttpHolder.OK_HTTP_CLIENT)
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
     }
+
+    private object RestServiceHolder {
+        internal val REST_SERVICE = RetrofitHolder
+            .RETROFIT_CLIENT
+            .create(RestService::class.java)
+    }
+
+    val restService: RestService get() = RestServiceHolder.REST_SERVICE
+
 }
